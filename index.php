6df110051a314faa5f81597807dd744efef7af51
Yellow Web <rattack@gmail.com>
2022-06-13 18:45:39 +0400

    $rawClick exists check

--- index.php
+++ index.php
@@ -3,11 +3,13 @@ require __DIR__.'/vendor/autoload.php';
 
 $abtest = new ABTest();
 $dom = $abtest->get_random_test_dom();
-$cId = $rawClick->getCampaignId();
-$subid = $rawClick->getSubId();
-if (isset($cId) && isset($subid)){
-    $keitaro=new KeitaroHelper($cId, $subid, $abtest->_testName);
-    $keitaro->update_click_params();
+if (isset($rawClick)){
+    $cId = $rawClick->getCampaignId();
+    $subid = $rawClick->getSubId();
+    if (isset($cId) && isset($subid)){
+        $keitaro=new KeitaroHelper($cId, $subid, $abtest->_testName);
+        $keitaro->update_click_params();
+    }
 }
 echo $dom->saveHTML();
 ?>
